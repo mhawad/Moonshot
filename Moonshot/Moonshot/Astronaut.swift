2ab787a5cf4f9dd99ac129d88d8f7b603651b1cd
Mirna Sokar <mirnahelmybio@yahoo.com>
2023-03-14 13:58:53 -0400

    Add files via upload

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Mirna Helmy on 3/13/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,35 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Mirna Helmy on 3/13/23.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+    
+}
